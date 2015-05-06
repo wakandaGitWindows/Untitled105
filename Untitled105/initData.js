e36822f36ff926ae028583b905091b138f4c3ade
wakandaGitWindows <wakanda.git.windows@gmail.com>
2015-05-06 12:05:22 +0200

    Update #3

---
+++ Untitled105/initData.js
@@ -0,0 +1,40 @@
+﻿/*
+* This file is part of Wakanda software, licensed by 4D under
+*  (i) the GNU General Public License version 3 (GNU GPL v3), or
+*  (ii) the Affero General Public License version 3 (AGPL v3) or
+*  (iii) a commercial license.
+* This file remains the exclusive property of 4D and/or its licensors
+* and is protected by national and international legislations.
+* In any event, Licensee's compliance with the terms and conditions
+* of the applicable license constitutes a prerequisite to any use of this file.
+* Except as otherwise expressly stated in the applicable license,
+* such license does not include any other license or rights on this file,
+* 4D's and/or its licensors' trademarks and/or other proprietary rights.
+* Consequently, no title, copyright or other proprietary rights
+* other than those specified in the applicable license is granted.
+*/
+function raz(){
+	
+	ds.Person.remove();
+	
+	wait(10);
+
+	ds.Person.setAutoSequenceNumber(1);
+}
+
+function buildData()
+{	raz();
+		
+	for (var i = 0; i<1000000; i++)
+	{	
+		var emp = new ds.Person(
+		{	
+			name:"name"+i		
+		});
+				
+		emp.save();		}
+}
+
+buildData();
+
+"all done";
